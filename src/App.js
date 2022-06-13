44621a14dd68982de9c63758617165a54dccdbab
robert <mainrobert04@gmail.com>
2022-06-13 13:42:50 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import Header from "./components/Header";
+import { useState } from "react";
+import Tasks from "./components/Tasks";
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor Appointment",
+      day: "feb 5th at 3:40pm",
+      remainder: true,
+    },
+    {
+      id: 2,
+      text: "Scholl Meeting",
+      day: "feb 5th at 3:40pm",
+      remainder: true,
+    },
+    { id: 3, text: "Shopping", day: "feb 5th at 3:40pm", remainder: false },
+  ]);
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,23 @@
-const Button = ({ color, text }) => {
+import PropTypes from "prop-types";
+
+const Button = ({ color, text, onClick }) => {
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button
+      style={{ backgroundColor: color }}
+      className="btn"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
+};
+Button.defaultProps = {
+  color: "steelBlue",
+};
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
