a8db2aa8e20bf98223ca4d41835c4fdda672e9da
robert <mainrobert04@gmail.com>
2022-06-13 14:21:06 +0300

    upadtes

--- src/App.js
+++ src/App.js
@@ -18,10 +18,14 @@ function App() {
     },
     { id: 3, text: "Shopping", day: "feb 5th at 3:40pm", remainder: false },
   ]);
+  //delete task
+  const deleteTask = (id) => {
+    console.log("delete,id");
+  };
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
