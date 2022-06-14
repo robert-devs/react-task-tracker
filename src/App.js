a2fc0f1d3356725d8b35a6823f480e7b5f0a4f74
robert <mainrobert04@gmail.com>
2022-06-14 13:16:25 +0300

    setting up reminder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import { useState } from "react";
 import Tasks from "./components/Tasks";
+import Task from "./components/Task";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -8,24 +9,36 @@ function App() {
       id: 1,
       text: "Doctor Appointment",
       day: "feb 5th at 3:40pm",
-      remainder: true,
+      reminder: true,
     },
     {
       id: 2,
       text: "Scholl Meeting",
       day: "feb 5th at 3:40pm",
-      remainder: true,
+      reminder: true,
     },
-    { id: 3, text: "Shopping", day: "feb 5th at 3:40pm", remainder: false },
+    { id: 3, text: "Shopping", day: "feb 5th at 3:40pm", reminder: false },
   ]);
   //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : Task
+      )
+    );
+  };
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "no thanks"
+      )}
     </div>
   );
 }
